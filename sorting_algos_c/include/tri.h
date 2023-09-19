58485b9bea6b4d63431949914d693749a0cfcbe1
Melville Danalet <melville.danalete@etu.unge.ch>
2023-09-20 00:25:16 +0200

    dossier prêt pour premier tri

---
+++ sorting_algos_c/include/tri.h
@@ -0,0 +1,12 @@
+#ifndef TRI_H
+#define TRI_H
+
+// Déclaration de la fonction de tri par insertion
+void tri_insertion(int arr[], int n);
+
+// Déclarations des autres fonctions
+// void tri_bulle(int arr[], int n);
+// void tri_fusion(int arr[], int n);
+// void tri_rapide(int arr[], int n);
+
+#endif // TRI_H
